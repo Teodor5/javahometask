e31511071c55f5b809a5cbdd0a54cbb83f695c51
pbod <pastushenko.bohdan@gmail.com>
2018-10-24 23:49:15 +0300

    guessletter

--- guessletter/src/main/java/guessletter/GuessTheLetter.java
+++ guessletter/src/main/java/guessletter/GuessTheLetter.java
@@ -1,33 +1,43 @@
 package guessletter;
-//https://github.com/yegor256/hangman приклад ООП проекту
 
 import javax.swing.*;
 import java.security.SecureRandom;
 import java.util.regex.Pattern;
 
-//TODO переробити в ООП
 
 public class GuessTheLetter {
+    private final char letter;
+
+    GuessTheLetter() {
+        letter = (char) (new SecureRandom().nextInt(26) + 'a');
+    }
+
+    char getLetter() {
+        return letter;
+    }
+
     public static void main(String[] args) {
 
-        char randomLetter = getRandomLowerCaseLatinLetter();
+        GuessTheLetter game = new GuessTheLetter();
+        game.start();
+    }
+
+    private void start() {
+        char randomLetter = this.getLetter();
         boolean userCorrect = false;
         int attemptsCount = 1;
-
         String userInputString;
         char userGuessedLetter;
+        //Ігровий цикл
         while (!userCorrect) {
-            attemptsCount++;
             String regex = "^[A-Za-z]$";
             do {
                 userInputString = JOptionPane.showInputDialog("Введіть, будь ласка, букву: ");
-                if(!Pattern.matches(regex, userInputString)){
+                if (!Pattern.matches(regex, userInputString)) {
                     JOptionPane.showMessageDialog(null, "ПІДКАЗКА\nЗа правилами гри ви повинні ввести одну латинську літеру");
                 }
             } while (!Pattern.matches(regex, userInputString));
             userGuessedLetter = userInputString.toLowerCase().charAt(0);
-
-
             if (userGuessedLetter > randomLetter) {
                 JOptionPane.showMessageDialog(null, "Введіть букву ближчу до початку алфавіту, спробуйте ще раз");
             } else if (userGuessedLetter < randomLetter) {
@@ -36,12 +46,8 @@ public class GuessTheLetter {
                 JOptionPane.showMessageDialog(null, "Вірно, ви відгадали букву \"" + randomLetter + "\". Ви впоралися за стільки спроб: " + attemptsCount + "!");
                 userCorrect = true;
             }
+            attemptsCount++;
         }
     }
 
-    private static char getRandomLowerCaseLatinLetter() {
-        SecureRandom r = new SecureRandom();
-        String alphabet = "abcdefghijklmnopqrstuvwxyz";
-        return alphabet.charAt(r.nextInt(alphabet.length()));
-    }
 }
