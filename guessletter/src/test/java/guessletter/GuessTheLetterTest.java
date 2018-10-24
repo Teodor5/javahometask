818291b537392e8ffb0f1ccfb8fb549ce409a7c4
pbod <pastushenko.bohdan@gmail.com>
2018-10-24 23:49:45 +0300

    guessletter

--- guessletter/src/test/java/guessletter/GuessTheLetterTest.java
+++ guessletter/src/test/java/guessletter/GuessTheLetterTest.java
@@ -1,11 +1,16 @@
 package guessletter;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.regex.Pattern;
 
 
 public  class GuessTheLetterTest {
     @Test
-    public void Test(){
-
+    public void generateLetterTest(){
+        GuessTheLetter game = new GuessTheLetter();
+        String regex = "^[a-z]$";
+        Assert.assertTrue("Перевірка входження згенерованї букви у діапазон [a-z]", Pattern.matches(regex, Character.toString(game.getLetter())));
     }
 }
